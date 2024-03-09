c56a9341cf48b898235a7d106fd1b29c386a9412
sakshamshahu <sakshamshahu.work@gmail.com>
2024-03-09 11:43:08 +0530

    feat: title
    Signed-off-by: sakshamshahu <sakshamshahu.work@gmail.com>

--- app/app/main.py
+++ app/app/main.py
@@ -2,4 +2,4 @@ import ollama
 import streamlit as st
 
 st.title('Codi: Unminify on command')
-st.write('Codi is a simple AI tool to unminify your code. Just paste your minified code and click on the button to unminify it.')
+st.write('Codi is a simple AI tool to unminify your code. Just paste your minified code and click on the button to unminify it. ')
